fe339b6eace07df95176cdf24b8324d5034b3faa
DanielGonz123 <91735919+DanielGonz123@users.noreply.github.com>
2021-12-02 14:23:51 +0000

    board created

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Daniel Gonzalez
+Ethan Gonzalez
+Hugo Dorta
+Connect 4
+December 13, 2021
+"""
+
+#imports numbrt python package, renamed np
+def create_board():
+  board = [ ["O"] * 6 ] * 7
+  for row in board:
+    print(row)
+
+create_board()
+
+""""
+def move():
+"""
