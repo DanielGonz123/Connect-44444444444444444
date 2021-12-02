4d28f2577c989216c0dda37e43494841f5fb31e2
DanielGonz123 <91735919+DanielGonz123@users.noreply.github.com>
2021-12-02 14:30:40 +0000

    list version of board

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ December 13, 2021
 
 #imports numbrt python package, renamed np
 def create_board():
-  board = [ ["O"] * 7 ] * 6
-  for row in board:
-    print(row)
+  row1 = ["O", "O", "O", "O", "O", "O", "O"]
+  row2 = ["O", "O", "O", "O", "O", "O", "O"]
+  row3 = ["O", "O", "O", "O", "O", "O", "O"]
+  row4 = ["O", "O", "O", "O", "O", "O", "O"]
+  row5 = ["O", "O", "O", "O", "O", "O", "O"]
+  row6 = ["O", "O", "O", "O", "O", "O", "O"]
+  print(row1)
+  print(row2)
+  print(row3)
+  print(row4)
+  print(row5)
+  print(row6)
 
 create_board()
-
-""""
+"""
 def move():
 """
